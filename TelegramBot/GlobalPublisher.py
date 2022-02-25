8c4f15fea3f66614bdbdf21eaf12f64c808b4960
Maksim Chmutov <maxtheboo1337@gmail.com>
2022-02-25 23:53:00 +0500

    Added image and audio messaging

--- TelegramBot/GlobalPublisher.py
+++ TelegramBot/GlobalPublisher.py
@@ -16,10 +16,39 @@ with open('token.txt', 'r') as f:
 updater = telegram.ext.Updater(TOKEN, use_context=True)
 disp = updater.dispatcher
 
-messageString = str()
 argumentCount = len(sys.argv)
-for i in range(1, argumentCount):
-    messageString += str(sys.argv[i]) + " "
 
-for user in users: 
-    updater.bot.send_message(chat_id = int(user),text = messageString)
+def sendTextMessage():
+    messageString = str()
+    for i in range(2, argumentCount):
+        messageString += str(sys.argv[i]) + " "
+
+    for user in users: 
+        updater.bot.send_message(chat_id = int(user),text = messageString)
+
+def sendImageURL():
+    for user in users: 
+        for i in range(2, argumentCount):
+            updater.bot.send_photo(chat_id = int(user),photo = sys.argv[i])
+
+def sendImageFile():
+    for user in users: 
+        for i in range(2, argumentCount):
+            updater.bot.send_photo(chat_id = int(user),photo = open(sys.argv[i], 'rb'))
+
+def sendAudioFile():
+    for user in users: 
+        for i in range(2, argumentCount):
+            updater.bot.send_audio(chat_id = int(user),audio = open(sys.argv[i], 'rb'))
+
+if(sys.argv[1] == "txt"):
+    sendTextMessage()
+elif(sys.argv[1] == "imgURL"):
+    sendImageURL()
+elif(sys.argv[1] == "img"):
+    sendImageFile()
+elif(sys.argv[1] == "audio"):
+    sendAudioFile()
+else:
+    print("Invalid format argument. Legal arguments: txt, img, imgURL, audio)")
+
